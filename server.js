70afb3c287ffb926bb02c5b44d20ce36445a0983
missalissa <alissa.beddow@gmail.com>
2017-12-10 11:22:48 -0800

    added env variables

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require('dotenv').config(); // allows us to use the environmental variables
 const express = require('express');
 const app = express();
 const pg = require('pg');
 // const fs = require('fs');
 // const data = require('./books.json');
 
-const PORT = 3000;
-const connString = 'postgres://@localhost:5432/book_list';
-const client = new pg.Client(connString);
+const PORT = process.env.PORT;
+// const connString = 'postgres://@localhost:5432/book_list';
+
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 // fs.readFile('./books.json', (err, JSON.stringify(data)) => {
@@ -20,7 +22,7 @@ app.get('/api/v1/books', (req, res) => {
         .then(data => res.send(data.rows));
 });
 
-app.get('api/v1/books/:user', (req, res) => {
+app.get('/api/v1/books/:user', (req, res) => {
     // will query db for cards sent to recipient
     // this is not showing up; need to add info here.
     res.send('will eventually send user book data');
